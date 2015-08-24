3be0d1f38aefc873b6cd15c3e103e5c7c32a33de
lykmapipo <lallyelias87@gmail.com>
2015-08-24 22:36:39 +0300

    adding response and request mock dependencies

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 //dependencies
 
-
-module.exports = function() {
-
+/**
+ * @description common HTTP responses for expressjs
+ * @param  {Object} options options to configure express-respond functions
+ * //options jsend,environment
+ * @return {Function}       a valid expressjs middleware
+ */
+module.exports = function(options) {
+    //cross check options
+    options = options || {};
 };
