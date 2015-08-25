677e393e435b148283b0e5d3a9d6a5ccc271e0a5
lykmapipo <lallyelias87@gmail.com>
2015-08-25 20:14:48 +0300

    update respond success specs

--- test/spec/success.js
+++ test/spec/success.js
@@ -2,6 +2,7 @@
 
 //dependencies
 var path = require('path');
+var faker = require('faker');
 var expect = require('chai').expect;
 var Request = require('mock-express-request');
 var Response = require('mock-express-response');
@@ -30,5 +31,109 @@ describe('respond success', function() {
 
             done();
         });
+    });
+
+    it('should be able to response with 201 `created`', function(done) {
+
+        var data = faker.helpers.contextualCard();
+
+        var request = new Request({
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        var response = new Response({
+            request: request,
+            finish: function() {
+
+                expect(response.statusCode).to.be.equal(201);
+
+                expect(response.statusMessage).to.be.equal('Created');
+
+                expect(response.get('content-type'))
+                    .to.be.equal('application/json; charset=utf-8');
+
+                expect(response._getJSON().name).to.equal(data.name);
+
+                done();
+            }
+        });
+
+        var respond = require(path.join(__dirname, '..', '..'))();
+
+        respond(request, response, function() {
+            //invoke created
+            response.created(data);
+
+        });
+
+    });
+
+    it('should be able to response with 200 `ok`', function(done) {
+
+        var data = faker.helpers.contextualCard();
+
+        var request = new Request({
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        var response = new Response({
+            request: request,
+            finish: function() {
+
+                expect(response.statusCode).to.be.equal(200);
+
+                expect(response.statusMessage).to.be.equal('OK');
+
+                expect(response.get('content-type'))
+                    .to.be.equal('application/json; charset=utf-8');
+
+                expect(response._getJSON().name).to.equal(data.name);
+
+                done();
+            }
+        });
+
+        var respond = require(path.join(__dirname, '..', '..'))();
+
+        respond(request, response, function() {
+            //invoke created
+            response.ok(data);
+
+        });
+
+    });
+
+    it('should be able to response with 204 `No Content`', function(done) {
+
+        var request = new Request({
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        var response = new Response({
+            request: request,
+            finish: function() {
+
+                expect(response.statusCode).to.be.equal(204);
+
+                expect(response.statusMessage).to.be.equal('No Content');
+
+                done();
+            }
+        });
+
+        var respond = require(path.join(__dirname, '..', '..'))();
+
+        respond(request, response, function() {
+            //invoke created
+            response.noContent();
+
+        });
+
     });
 });
