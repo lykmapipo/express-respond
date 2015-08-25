0768b0a982c2b657125d424e5aa4412fc2b48dc1
lykmapipo <lallyelias87@gmail.com>
2015-08-25 13:09:49 +0300

    fix html content type response

--- test/negotiate.js
+++ test/negotiate.js
@@ -2,6 +2,7 @@
 
 //dependencies
 var path = require('path');
+var ejs = require('ejs');
 var faker = require('faker');
 var expect = require('chai').expect;
 var Request = require('mock-express-request');
@@ -43,6 +44,51 @@ describe('response content negotiation', function() {
 
             //invoke negotiated
             negotiated(data);
+
+        });
+
+    });
+
+    it('should be able to negotiate with html response type', function(done) {
+
+        var request = new Request({
+            headers: {
+                'Accept': 'text/html'
+            }
+        });
+
+        var response = new Response({
+            render: ejs.renderFile,
+            request: request,
+            finish: function() {
+
+                expect(response.get('content-type'))
+                    .to.be.equal('text/html; charset=utf-8');
+
+                expect(response._getString()).to.be.equal('<p>Hello Mock</p>');
+
+                done();
+            }
+        });
+
+        var negotiate = require(path.join(__dirname, '..', 'lib', 'negotiate'));
+
+        var respond = require(path.join(__dirname, '..'))();
+
+        respond(request, response, function() {
+
+            var negotiated = negotiate(request, response);
+
+            expect(negotiated).to.be.a('function');
+
+            expect(request._respond.defaultType).to.equal('json');
+            expect(request._respond.environment).to.equal('development');
+            expect(request._respond.types).to.eql(['json', 'html', 'text']);
+
+            //invoke negotiated
+            negotiated(path.join(__dirname, 'template.ejs'), {
+                name: 'Mock'
+            });
 
         });
 
