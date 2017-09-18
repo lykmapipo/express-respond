c13f0b6270187918fb3a0a6eb7cfe0a1aef8f28e
lykmapipo <lallyelias87@gmail.com>
2017-09-18 14:13:25 +0300

    improve specs for forced content type

--- test/unit/negotiate.js
+++ test/unit/negotiate.js
@@ -242,4 +242,45 @@ describe('response content negotiation', function() {
 
     });
 
+    it('should always negotiate with json response type if forced', function(done) {
+
+        var data = faker.helpers.contextualCard();
+
+        var request = new Request({});
+
+        var response = new Response({
+            request: request,
+            finish: function() {
+
+                expect(response.get('content-type'))
+                    .to.be.equal('application/json; charset=utf-8');
+
+                expect(response._getJSON().name).to.equal(data.name);
+                done();
+            }
+        });
+
+        var negotiate = require(path.join(__dirname, '..', '..', 'lib', 'negotiate'));
+
+        var respond = require(path.join(__dirname, '..', '..'))({
+            types: ['json']
+        });
+
+        respond(request, response, function() {
+
+            var negotiated = negotiate(request, response);
+
+            expect(negotiated).to.be.a('function');
+
+            expect(request._respond.defaultType).to.equal('json');
+            expect(request._respond.environment).to.equal('development');
+            expect(request._respond.types).to.eql(['json']);
+
+            //invoke negotiated
+            negotiated(data);
+
+        });
+
+    });
+
 });
